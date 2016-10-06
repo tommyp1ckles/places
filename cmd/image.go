1f1283588438f0a6e3af5720c75a938a0ab8038c
Tom <101188h@acadiau.ca>
2016-10-05 23:31:18 -0300

    When finding image for multiple images the print message.

--- cmd/image.go
+++ cmd/image.go
@@ -10,12 +10,15 @@ import (
 var (
 	SingleFileCmds = &cobra.Command{
 		Use:   "image",
-		Short: "<image_path>",
+		Short: "<image_path> ...",
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) < 1 {
 				cmd.Help()
 			}
 			for _, path := range args {
+				if len(args) > 1 {
+					fmt.Printf("Fetching places for %s\n", path)
+				}
 				err := places.Image(path)
 				if err != nil {
 					fmt.Println("Error: ", err)
